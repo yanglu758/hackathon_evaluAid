815af3d1ce71c5fa1cbbe6eef140be3fdbff0e07
Lu Yang <young07ly@hotmail.com>
2016-08-27 13:23:20 -0700

    Updated HomeHandler and templates importing

--- controllers/HomeHandler.py
+++ controllers/HomeHandler.py
@@ -1,5 +1,12 @@
 import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname('oneclick0827'), 'templates')),
+)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
